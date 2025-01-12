54447f740e68ac9876d175b7ed33f8ae2aeeb856
mdholder74 <mdholder74@gmail.com>
2025-01-11 19:51:58 -0500

    created GET routes in the combatRoutes file

--- routes/combatRoutes.js
+++ routes/combatRoutes.js
@@ -9,3 +9,41 @@ const Fight = require('../models/fights');
 
 //EXPORT ROUTER
 module.exports = router;
+
+//ALL GET ROUTES
+
+//GET EVENTS ROUTE (Full Route localhost:1000/api/combat/events)
+router.get('/events', async (req, res) => {
+    try {
+        const events = await Event.find({});
+        res.json(events);
+    }
+    catch (error) {
+        console.error(`Something went wrong: ${error.message}`);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+//GET FIGHTERS ROUTE (Full Route localhost:1000/api/combat/fighters)
+router.get('/fighters', async (req, res) => {
+    try {
+        const fighters = await Fighter.find({});
+        res.json(fighters);
+    }
+    catch (error) {
+        console.error(`Something went wrong: ${error.message}`);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+//GET FIGHTS ROUTE (Full Route localhost:1000/api/combat/fights)
+router.get('/fights', async (req, res) => {
+    try {
+        const fights = await Fight.find({});
+        res.json(fights);
+    }
+    catch (error) {
+        console.error(`Something went wrong: ${error.message}`);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
